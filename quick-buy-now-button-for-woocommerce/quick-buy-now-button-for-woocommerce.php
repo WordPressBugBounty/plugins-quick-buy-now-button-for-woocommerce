edec808c2b10c0b45e467e7f95c1e23e514977c6
Sean Murphy <sean@iamseanmurphy.com>
2025-09-13 05:17:53 +0000

    1.0.16

--- quick-buy-now-button-for-woocommerce/quick-buy-now-button-for-woocommerce.php
+++ quick-buy-now-button-for-woocommerce/quick-buy-now-button-for-woocommerce.php
@@ -4,7 +4,7 @@
 * Plugin URI: https://wordpress.org/plugins/quick-buy-now-button-for-woocommerce/
 * Description: Makes your customers' checkout process easier and faster and allows you to redirect customers directly to the checkout, cart or any external link for quick purchase.
 * Author: Tanvirul Haque
-* Version: 1.0.15
+* Version: 1.0.16
 * Author URI: https://wpxpress.net
 * Text Domain: woo-buy-now-button
 * Domain Path: /languages
@@ -12,7 +12,7 @@
 * Requires PHP: 7.4
 * Requires at least: 4.8
 * Tested up to: 6.8
-* WC tested up to: 9.8
+* WC tested up to: 10.1
 * WC requires at least: 4.6
 * License: GPLv2+
 */
@@ -20,7 +20,7 @@
 defined( 'ABSPATH' ) or die( 'Keep Silent' );
 
 if ( ! defined( 'WOO_BUY_NOW_BUTTON_PLUGIN_VERSION' ) ) {
-	define( 'WOO_BUY_NOW_BUTTON_PLUGIN_VERSION', '1.0.15' );
+	define( 'WOO_BUY_NOW_BUTTON_PLUGIN_VERSION', '1.0.16' );
 }
 
 if ( ! defined( 'WOO_BUY_NOW_BUTTON_PLUGIN_FILE' ) ) {
--- quick-buy-now-button-for-woocommerce/readme.txt
+++ quick-buy-now-button-for-woocommerce/readme.txt
@@ -5,8 +5,8 @@ Requires PHP: 7.4
 Requires at least: 4.8
 Tested up to: 6.8
 WC requires at least: 4.5
-WC tested up to: 9.8
-Stable tag: 1.0.15
+WC tested up to: 10.1
+Stable tag: 1.0.16
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -110,6 +110,9 @@ If you are unable to use any of the methods due to internet connectivity and fil
 7. Activate the plugin.
 
 == Changelog ==
+
+= 1.0.16 =
+* Add - WooCommerce 10.1+ compatibility
 
 = 1.0.15 =
 * Add - WordPress 6.8+ compatibility
